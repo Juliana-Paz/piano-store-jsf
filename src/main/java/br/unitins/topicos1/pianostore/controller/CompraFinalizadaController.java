c5c51ee737f9e499743f9d1a011188561ea78ec2
Igor Lourenço <igorlourenco.dev@gmail.com>
2022-12-02 22:43:44 -0300

    Criar Tela de Compra Finalizada

---
+++ src/main/java/br/unitins/topicos1/pianostore/controller/CompraFinalizadaController.java
@@ -0,0 +1,37 @@
+package br.unitins.topicos1.pianostore.controller;
+
+import java.io.Serializable;
+
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import br.unitins.topicos1.pianostore.model.Compra;
+
+@Named
+@ViewScoped
+public class CompraFinalizadaController implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5517921013232067512L;
+
+	private Compra compra;
+
+	public CompraFinalizadaController() {
+		Flash flash = FacesContext.getCurrentInstance().getExternalContext().getFlash();
+		flash.keep("flashCompraFinalizada");
+		setCompra((Compra) flash.get("flashCompraFinalizada"));
+	}
+
+	
+	public Compra getCompra() {
+		return compra;
+	}
+
+	public void setCompra(Compra compra) {
+		this.compra = compra;
+	}
+
+}
